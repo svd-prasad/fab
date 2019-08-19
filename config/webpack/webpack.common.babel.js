57a14b6b18f3b8c2f5bd8c0dfaaff4c0bad7082f
prasad_rivigo <prasad.k@fptechscience.com>
2019-08-19 17:38:15 +0530

    fab coce

---
+++ config/webpack/webpack.common.babel.js
@@ -0,0 +1,59 @@
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+import paths from './paths';
+import rules from './rules';
+
+module.exports = {
+    entry: "/Users/prasadkotikalapudi/Downloads/react-bolt-master/src/index.js",
+    module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+    resolve: {
+        modules: ['src', 'node_modules'],
+        extensions: ['*', '.js', '.scss', '.css']
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: "/Users/prasadkotikalapudi/Downloads/react-bolt-master/src/index.html",
+            minify: {
+                collapseInlineTagWhitespace: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: true,
+                minifyURLs: true,
+                removeComments: true,
+                removeAttributeQuotes: true
+            }
+        })
+    ]
+};
